53a784a734d47e06a77fac8fe9a5f2fdf92cc226
Gideon Singer <72046459+TheDigitalGarbologist@users.noreply.github.com>
2024-06-15 14:09:17 -0400

    Update Earthquake_Demo.py

--- Earthquake_Demo.py
+++ Earthquake_Demo.py
@@ -34,7 +34,7 @@ def transform_data(data):
 
 # Function to create a Folium map with stylized animated markers
 def create_folium_map(df):
-    m = folium.Map(location=[df['Latitude'].mean(), df['Longitude'].mean()], zoom_start=2)
+    m = folium.Map(location=[0,0], zoom_start=2)
     
     # Add a tile layer for ESRI Imagery
     folium.TileLayer(
