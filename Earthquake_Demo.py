ce5d903599991764af0b3341e2f58b6c67af8ed7
Gideon Singer <72046459+TheDigitalGarbologist@users.noreply.github.com>
2024-06-16 12:49:02 -0400

    Update Earthquake_Demo.py

--- Earthquake_Demo.py
+++ Earthquake_Demo.py
@@ -1,4 +1,4 @@
-import streamlit as st
+eimport streamlit as st
 import pandas as pd
 import folium
 from streamlit_folium import folium_static
@@ -52,7 +52,7 @@ def transform_data(data):
 
 # Function to create a Folium map with stylized animated markers
 def create_folium_map(df):
-    m = folium.Map(location=[0,0], zoom_start=2)
+    m = folium.Map(location=[0,0], zoom_start=1)
     # Add a tile layer for ESRI Imagery
     folium.TileLayer(
         tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
